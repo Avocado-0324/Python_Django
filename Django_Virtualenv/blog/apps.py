12510685db5c020e1227a1c2eb4ee022e5ad417e
Avocado0324 <niqinou@gmail.com>
2020-10-28 10:44:00 +0900

    delete Django_Virtualenv

--- Django_Virtualenv/blog/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-
-class BlogConfig(AppConfig):
-    name = 'blog'
-    verbose_name = '博客'
-    verbose_name_plural = verbose_name
--- Django_Virtualenv/comments/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CommentsConfig(AppConfig):
-    name = 'comments'
-    verbose_name = "评论"  # 让comments应用在admin后台显示中文。
